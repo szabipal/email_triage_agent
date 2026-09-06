239ed86da700e9e8384bbff9f7e9e9221a8382b8
szabipal <szabi.pal.2000@gmail.com>
2026-09-06 13:20:04 +0200

    Add Chroma vector index adapter

--- src/email_agent/retrieval/__init__.py
+++ src/email_agent/retrieval/__init__.py
@@ -4,10 +4,14 @@ from email_agent.retrieval.embeddings import (
     EmbeddingRequest,
     FakeEmbeddingProvider,
 )
+from email_agent.retrieval.vector_store import ChromaIndex, VectorDocument, VectorMatch
 
 __all__ = [
+    "ChromaIndex",
     "Embedding",
     "EmbeddingProvider",
     "EmbeddingRequest",
     "FakeEmbeddingProvider",
+    "VectorDocument",
+    "VectorMatch",
 ]
