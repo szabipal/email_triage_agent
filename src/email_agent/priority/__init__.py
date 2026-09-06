8a260c779ec46f7832066f7362b3834da8937e2a
szabipal <szabi.pal.2000@gmail.com>
2026-09-06 13:01:14 +0200

    Add priority weight configuration

--- src/email_agent/priority/__init__.py
+++ src/email_agent/priority/__init__.py
@@ -1,3 +1,15 @@
+from email_agent.priority.config import (
+    DEFAULT_PRIORITY_THRESHOLDS,
+    DEFAULT_PRIORITY_WEIGHTS,
+    PriorityScoringConfig,
+    load_priority_config,
+)
 from email_agent.priority.model import PriorityScoringInput
 
-__all__ = ["PriorityScoringInput"]
+__all__ = [
+    "DEFAULT_PRIORITY_THRESHOLDS",
+    "DEFAULT_PRIORITY_WEIGHTS",
+    "PriorityScoringConfig",
+    "PriorityScoringInput",
+    "load_priority_config",
+]
